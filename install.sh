502db20be29519b28070abf40dd76c2d5297f969
Wiseluster <chinaware@foxmail.com>
2016-12-08 12:43:32 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-mkdir -p ${GOPATH:="${HOME}/go"}/src
+GOPATH=${GOPATH:-"${HOME}/go"}
+
+mkdir -p ${GOPATH}/src
 cp -fR ${PWD}/src ${GOPATH}
 ${GO:="go"} install wiselusterlab/container/stack
